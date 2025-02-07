601d5d4f12c39d32ca670b14f78166511fc4c6e5
Maciej Piechowski <58206@etu.he2b.be>
2025-02-07 15:08:37 +0100

    ch3 continuation

---
+++ ch3/greatest_common_divisor.py
@@ -0,0 +1,6 @@
+import math
+
+print("Welcome!")
+n1 = int(input("Enter a first number : "))
+n2 = int(input("Enter a second number : "))
+print("This is the greatest common divisor : ", math.gcd(n1,n2))
