3b3cdaa139b03370c754fd8bb62fed9a63cc9a9a
Maciej Piechowski <58206@etu.he2b.be>
2025-02-07 18:42:45 +0100

    ch3 end

---
+++ ch3/generate_random.py
@@ -0,0 +1,6 @@
+import random
+
+print("Welcome!")
+n1 = int(input("Enter a first number : "))
+n2 = int(input("Enter a second number : "))
+print("Random number between ",n1 ," and ", n2 ," : ", random.randint(n1,n2))
---
+++ ch3/sort.py
@@ -0,0 +1,9 @@
+print("Welcome!")
+
+list = []
+
+for x in range(4):
+    list.append(int(input("Enter a number : ")))
+
+print(list)
+print(sorted(list))
